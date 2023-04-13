f797a44717f3bcb97ab9eead9d816dc9845c3f9c
Viktor Gamov <gamussa@gmail.com>
2023-04-13 20:40:35 +0200

    fixing SM meeting example

--- meeting/src/main/java/sh/kongme/developer/meeting/MeetingApplication.java
+++ meeting/src/main/java/sh/kongme/developer/meeting/MeetingApplication.java
@@ -1,7 +1,10 @@
 package sh.kongme.developer.meeting;
 
+import org.apache.coyote.Response;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,16 +22,19 @@ import static java.lang.Thread.*;
 public class MeetingApplication {
 
   @GetMapping("/meet")
-  public void meeting(@RequestHeader Map<String, String> headers) {
+  public ResponseEntity<String> meeting(@RequestHeader Map<String, String> headers) {
 
     log.info("Client Headers: {}", headers);
 
     log.info("\uD83D\uDC68\u200D\uD83C\uDFED gone meeting...");
+    
     try {
       sleep(250L);
+      return new ResponseEntity<>("✅ done...", HttpStatus.OK);
     } catch (InterruptedException e) {
       // ¯\_( ͡° ͜ʖ ͡°)_/¯
     }
+    return null;
   }
 
 
