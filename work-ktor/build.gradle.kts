1add4856d44f188e5710fa4e0c15cf56a60988cf
Viktor Gamov <gamussa@gmail.com>
2023-05-09 11:52:39 +0200

    update dependencies

--- work-ktor/build.gradle.kts
+++ work-ktor/build.gradle.kts
@@ -3,8 +3,8 @@ val kotlin_version: String by project
 val logback_version: String by project
 
 plugins {
-    kotlin("jvm") version "1.8.20"
-    id("io.ktor.plugin") version "2.2.4"
+    kotlin("jvm") version "1.8.21"
+    id("io.ktor.plugin") version "2.3.0"
 }
 
 group = "builders.kong"
